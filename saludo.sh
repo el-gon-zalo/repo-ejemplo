c40380d22362aef5e129417ef239306a8b79924a
el-gon-zalo <gonzalo.ri.j@gmail.com>
2025-01-16 19:10:07 -0300

    hicimos un archivo bash de manejo de archivos

---
+++ saludo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "hola a todos, la fecha actual es:$(date)"
+#copia un archivo de un directorio a otro
+#cp hola.txt hola2.txt
+#renombrar un archivo
+#mv hola2.txt copia.txt
+mkdir subdir
+mv copia.txt subdir/copia.txt
